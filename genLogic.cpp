b30c1227d0f1667cd27b8f565afe49aae83fa11a
anonymous <anonymous>
2019-09-29 17:40:43 -0400

    Created function for inner product

---
+++ genLogic.cpp
@@ -0,0 +1,17 @@
+//SET OF FILES TO SERVE AS COMMON LOGIC FOR THE UDFs
+extern "C" {
+#include  "../Ext_Files/gen_matrices.h"
+#include  "../Ext_Files/cryptorand.c"
+}
+#include <vector>
+
+long innerProduct(vector<int> x , vector<int> y){
+	long sum = 0;
+	if(x.size() != y.size()){
+		return 1;
+	}
+	for(int i = 0; i< x-> _elems; i++){
+		sum += x[i]*y[i];
+	}
+	return sum;
+}
