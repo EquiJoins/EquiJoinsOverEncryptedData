a13e81e2411097e54a4fefa70f8809974e4e333a
anonymous <anonymous>
2019-12-05 15:36:26 -0500

    Updated functions that were in a separate directory

--- UDF.cpp
+++ UDF.cpp
@@ -38,6 +38,8 @@ static pthread_mutex_t LOCK_hostname;
 #endif
 #include <pbc/pbc.h>
 #include <functional>
+//OBSOLETE TEST CODE TO ENSURE THAT MY SQL UDF SYSTEM WORKS
+/**
 my_bool hello_world_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
 char* hello_world(UDF_INIT *initid, UDF_ARGS *args,
           char *result, unsigned long *length,
@@ -56,8 +58,34 @@ char* hello_world(UDF_INIT *initid, UDF_ARGS *args,
 		*length = 13;
 		return result;
 }
-void gen_polynomial(int size,int* roots){
-	
+**/
+struct data{
+	int* beta;
+	int target;
+};
+
+int* gen_polynomial(int size,int* roots){
+	int size = sizeof(roots)/sizeof(int);
+	int* ret = new int[size+1];
+	for(int i = 0; i< size+1; i++){
+		ret[i] = 1;
+	}
+	ret[1] = roots[0];
+	for(int i = 1; i<size; i++){
+		for(int a = 0; a<i; i++){
+			ret[a+1] += root[i]*ret[a];
+		}
+	}
+	return ret;
+}
+
+int innerProd(int* alpha, int* beta){
+	int size = sizeof(alpha)/sizeof(int);
+	int ret = 0;
+	for(int i = 0; i< size; i++){
+		ret += alpha[i] * beta[i];
+	}
+	return ret;
 }
 void recursive_generation(int* roots, int* results, int count){
 	if(count == size(roots)/sizeof(int)) return results;
@@ -88,16 +116,54 @@ bool check_init(UDF_INIT *initid, UDF_ARGS *args, char *message){
 	char init[]="type d\nq 625852803282871856053922297323874661378036491717\nn 625852803282871856053923088432465995634661283063\nh 3\nr 208617601094290618684641029477488665211553761021\na 581595782028432961150765424293919699975513269268\nb 517921465817243828776542439081147840953753552322\nk 6\nnk 60094290356408407130984161127310078516360031868417968262992864809623507269833854678414046779817844853757026858774966331434198257512457993293271849043664655146443229029069463392046837830267994222789160047337432075266619082657640364986415435746294498140589844832666082434658532589211525696\nhk 1380801711862212484403205699005242141541629761433899149236405232528956996854655261075303661691995273080620762287276051361446528504633283152278831183711301329765591450680250000592437612973269056\ncoeff0 472731500571015189154958232321864199355792223347\ncoeff1 352243926696145937581894994871017455453604730246\ncoeff2 289113341693870057212775990719504267185772707305\nnqr 431211441436589568382088865288592347194866189652";
 	element_t h;
 	pairing_init_set_str(pairing, init);
+	int r[20];
+	element_from_hash(h,args[0],size(args[0])/sizeof(char));
+	int alpha[21];
+	alpha[0] = element_to_bytes_x_only(h); // sets element 0 of the alpha vector to H(a)
 	element_from_hash(h,args[1],size(args[1])/sizeof(char));
+	r[0] = element_to_bytes_x_only(h);
+	for(int i = 20; !(i<0); i--){
+		element_t temp;
+		element_t count;
+		mpz_t c;
+		element_set_si(count,i);
+		element_to_mpz(c,count);
+		element_pow_mpz(temp,h,c);
+		alpha[i] = element_to_bytes_x_only(temp);
+	}
+	element_from_hash(h,args[1],size(args[1])/sizeof(char));
+	for(int i = 1; i<20; i++){
+		element_random(h);
+		r[i] = element_to_bytes_x_only(h);
+	}
+	int* pol = gen_polynomial(r);
+	int* beta = new int[21];
+	element_random(h);
+	beta[0] = element_to_bytes_x_only(h);
+	for(int i = 1; i< 21; i++){
+		beta[i] = pol[i-1];
+	}
+	delete pol;
 
-
+	int target = innerProd(alpha,beta);
+	data* data = new data();
+	data -> target = target;
+	data -> beta = beta;
+	initid -> ptr = data;
 }
 void check_deinit(UDF_INIT *initid){
-
+	delete initid -> ptr -> beta;
+	delete initid -> ptr;
 }
 long long check(UDF_INIT *initid, UDF_ARGS *args,char *is_null, char *error){
-	void* internal_args = initid -> ptr; //format of internal args is internal_args[0] = target_inner_prod, internal_args[1] = beta vector
-
+	data* internal_args = initid -> ptr; //format of internal args is internal_args[0] = target_inner_prod, internal_args[1] = beta vector
+	int* val;
+	memset(val, args[2], sizeof(args[2]));
+	int target = internal_args -> target;
+	int* beta = internal_args -> beta;
+	int result = innerProd(beta,val);
+	if(result == target) return 1;
+	else return 0;
 }
 
 #endif /* HAVE_DLOPEN */
