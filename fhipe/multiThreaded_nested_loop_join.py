1366c41c0e700db11b620424068bfc917aca9ca3
anonymous <anonymous>
2020-07-28 12:11:36 -0400

    Checked in new test harness and new renamed join multithread to nested loop join

--- fhipe/multiThreaded_nested_loop_join.py
+++ fhipe/multiThreaded_nested_loop_join.py
@@ -1,30 +1,20 @@
-import sys, os
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(1, os.path.abspath('..'))
-
 from fhipe import ipe
 from timeit import default_timer as timer
 import multiprocessing,resource
+import sys
 #Query being run is
 # SELECT * FROM a, b WHERE b.x = 3
-def join_b_thread(pp, tag2, ct2,b_pt_attributes,b_enc_attributes,q,count):
+def join_b_thread(pp, tag2, ct2, b_pt_attributes, b_enc_attributes, q, count):
 	for y in range(0,len(b_pt_attributes)):
-			if((b_pt_attributes[y])[1] == str(3)):
+			if((b_pt_attributes[y])[0] == str(3)):
 				(tag1, ct1) = b_enc_attributes[y];
 				if(ipe.decrypt(pp, tag2, ct2) == ipe.decrypt(pp, tag1, ct1)):
 					q.put(count);
 	q.put(-1);
-def inner_join(a_table,b_table):
+def inner_join(a_enc_attributes,a_pt_attributes,b_enc_attributes,b_pt_attributes,pp):
 	ret = []
-	count = 0;
-	a_enc_attributes = [];
-	b_enc_attributes = [];
-	a_pt_attributes = [];
-	b_pt_attributes = [];
 	threads = [];
 	queues = [];
-	target_values = [str(3),str(4),str(1)]
-	padding = []
 
 	#This is the actual join code
 	for x in range(0,len(a_pt_attributes)):
@@ -47,9 +37,3 @@ def inner_join(a_table,b_table):
 	print("Peak memory usage "+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/resource.getpagesize()))
 	print(str(end - start)+"\n")
 	print(ret)
-opt = sys.argv
-if(len(opt) != 3):
-	print("Expecting 2 input tables")
-	exit(-1)
-
-inner_join(open(opt[1]),open(opt[2]))
