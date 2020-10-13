65937698b01e12eb8c04778be374cfece9cda2d9
anonymous <anonymous>
2020-10-13 16:04:30 -0400

    clean up hash based impl

--- hash_based_impl/hash_based_join.py
+++ hash_based_impl/hash_based_join.py
@@ -1,3 +1,7 @@
+import sys,os
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(1, os.path.abspath('..'))
+
 from charm.toolbox.pairinggroup import ZR
 from fhipe import ipe
 import csv
@@ -13,10 +17,9 @@ def read_table(infile):
 # SELECT * FROM A
 # INNER JOIN B ON A.a = B.b
 # WHERE A.x IN (x_c) AND B.y IN (y_c)
-# such that a and b are primary keys
+# such that a is a primary key, and b is the corresponding foreign key
 # returns the count of resulting rows
 def hash_based_join(table_a_file, a, x, x_c, table_b_file, b, y, y_c):
-  count = 0
   (pp, msk) = ipe.setup(6)
   (detB, B, Bstar, group, g1, g2) = msk
 
@@ -31,21 +34,22 @@ def hash_based_join(table_a_file, a, x, x_c, table_b_file, b, y, y_c):
   encrypted_table_b = ipe.encryptTable(msk, table_b, b, y)
   hash_table = {ipe.decrypt(pp, k_a, c_a):0 for c_a in encrypted_table_a}
 
+  count = 0
   for c_b in encrypted_table_b:
     if ipe.decrypt(pp, k_b, c_b) in hash_table:
       count += 1
 
   return count
 
-table_a_file = ""
+table_a_file = "table_a.in"
 a = "a_id" 
 x = "a_val"
 x_c = "1" 
 
-table_b_file = ""
-b = "b_id"
+table_b_file = "table_b.in"
+b = "a_id"
 y = "b_val"
 y_c = "1" 
 
 count = hash_based_join(table_a_file, a, x, x_c, table_b_file, b, y, y_c)
-print(count + " resulting rows")
+print('{} resulting rows.'.format(count))
