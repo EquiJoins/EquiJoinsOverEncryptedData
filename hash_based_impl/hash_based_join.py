7ccd495920405856381a371b9a138d7e89677e4f
anonymous <anonymous>
2020-12-03 04:25:13 -0500

    Completing experiments

--- hash_based_impl/hash_based_join.py
+++ hash_based_impl/hash_based_join.py
@@ -60,7 +60,7 @@ def hash_based_join(pp, msk, encrypted_table_a, encrypted_table_b, x_c, y_c):
 # NOTE: you must add a row with column heads to the tables that you will use. one should use the
 # attribute names used here: http://www.tpc.org/tpc_documents_current_versions/pdf/tpc-h_v2.17.1.pdf#page=13
 
-def experiment(pp, msk, sf, iters):
+def experiment_1(pp, msk, sf, iters):
   print("--------------------------- sf="+sf)
   
   table_a_file = "data/"+sf+"/customer.tbl"
@@ -146,15 +146,120 @@ def experiment(pp, msk, sf, iters):
       print('num matches: {}'.format(len(matches)))
       print('')
 
+
+def experiment_2(in_clause_max_size, iters):
+  print("--------------------------- in_clause_max_size={}".format(in_clause_max_size))
+  global IN_CLAUSE_MAX_SIZE
+  IN_CLAUSE_MAX_SIZE = in_clause_max_size
+  (pp, msk) = ipe.setup(3+IN_CLAUSE_MAX_SIZE+1)
+  
+  table_a_file = "data/0.01/customer.tbl"
+  a = "custkey" 
+  pk_a = a
+  x = "selectivity"
+
+  table_b_file = "data/0.01/orders.tbl"
+  b = "custkey"
+  pk_b = "orderkey"
+  y = "selectivity"
+
+  print('starting to encrypt customer')
+  table_a = read_table(table_a_file, '|')
+  encrypted_table_a = ipe.encryptTable(msk, table_a, pk_a, a, x, IN_CLAUSE_MAX_SIZE)
+  print('done encrypting customer')
+
+  print('starting to encrypt orders')
+  table_b = read_table(table_b_file, '|')
+  encrypted_table_b = ipe.encryptTable(msk, table_b, pk_b, b, y, IN_CLAUSE_MAX_SIZE)
+  print('done encrypting orders')
+
+  print('')
+  for i in range(iters):
+    # selectivity = 1/100
+    x_c = ["100"]
+    y_c = ["100"]
+    query_start = time.time()
+    (matches, decryptions) = hash_based_join(pp, msk, encrypted_table_a, encrypted_table_b, x_c, y_c)
+    query_end = time.time()
+    print('selectivity=1/100 took: {}s overall'.format(query_end - query_start))
+    print('time spent NOT doing decryption: {}s'.format((query_end - query_start) - sum(decryptions)))
+    print('time per decryption took: {}ms'.format((sum(decryptions) / len(decryptions)) * 1000))
+    print('num matches: {}'.format(len(matches)))
+    print('')
+
+    # selectivity = 1/50
+    x_c = ["50"]
+    y_c = ["50"]
+    query_start = time.time()
+    (matches, decryptions) = hash_based_join(pp, msk, encrypted_table_a, encrypted_table_b, x_c, y_c)
+    query_end = time.time()
+    print('selectivity=1/50 took: {}s overall'.format(query_end - query_start))
+    print('time spent NOT doing decryption: {}s'.format((query_end - query_start) - sum(decryptions)))
+    print('time per decryption took: {}ms'.format((sum(decryptions) / len(decryptions)) * 1000))
+    print('num matches: {}'.format(len(matches)))
+    print('')
+
+    # selectivity = 1/25
+    x_c = ["25"]
+    y_c = ["25"]
+    query_start = time.time()
+    (matches, decryptions) = hash_based_join(pp, msk, encrypted_table_a, encrypted_table_b, x_c, y_c)
+    query_end = time.time()
+    print('selectivity=1/25 took: {}s overall'.format(query_end - query_start))
+    print('time spent NOT doing decryption: {}s'.format((query_end - query_start) - sum(decryptions)))
+    print('time per decryption took: {}ms'.format((sum(decryptions) / len(decryptions)) * 1000))
+    print('num matches: {}'.format(len(matches)))
+    print('')
+
+    # selectivity = 1/12.5
+    x_c = ["12.5"]
+    y_c = ["12.5"]
+    query_start = time.time()
+    (matches, decryptions) = hash_based_join(pp, msk, encrypted_table_a, encrypted_table_b, x_c, y_c)
+    query_end = time.time()
+    print('selectivity=1/12.5 took: {}s overall'.format(query_end - query_start))
+    print('time spent NOT doing decryption: {}s'.format((query_end - query_start) - sum(decryptions)))
+    print('time per decryption took: {}ms'.format((sum(decryptions) / len(decryptions)) * 1000))
+    print('num matches: {}'.format(len(matches)))
+    print('')
+
+    # selectivity = 1/1
+    if selectivity_1:
+      x_c = []
+      y_c = []
+      query_start = time.time()
+      (matches, decryptions) = hash_based_join(pp, msk, encrypted_table_a, encrypted_table_b, x_c, y_c)
+      query_end = time.time()
+      print('selectivity=1/1 took: {}s overall'.format(query_end - query_start))
+      print('time spent NOT doing decryption: {}s'.format((query_end - query_start) - sum(decryptions)))
+      print('time per decryption took: {}ms'.format((sum(decryptions) / len(decryptions)) * 1000))
+      print('num matches: {}'.format(len(matches)))
+      print('')
+
 # MAIN
+# experiments to test relationship between overall time and scale factor
+"""
 (pp, msk) = ipe.setup(3+IN_CLAUSE_MAX_SIZE+1)
-experiment(pp, msk, "0.01", 5)
-experiment(pp, msk, "0.02", 5)
-experiment(pp, msk, "0.03", 5)
-experiment(pp, msk, "0.04", 5)
-experiment(pp, msk, "0.05", 5)
-experiment(pp, msk, "0.06", 5)
-experiment(pp, msk, "0.07", 5)
-experiment(pp, msk, "0.08", 5)
-experiment(pp, msk, "0.09", 5)
-experiment(pp, msk, "0.1", 5)
+experiment_1(pp, msk, "0.01", 5)
+experiment_1(pp, msk, "0.02", 5)
+experiment_1(pp, msk, "0.03", 5)
+experiment_1(pp, msk, "0.04", 5)
+experiment_1(pp, msk, "0.05", 5)
+experiment_1(pp, msk, "0.06", 5)
+experiment_1(pp, msk, "0.07", 5)
+experiment_1(pp, msk, "0.08", 5)
+experiment_1(pp, msk, "0.09", 5)
+"""
+
+# experiments to test relationship between overall time and IN_CLAUSE_MAX_SIZE
+"""
+experiment_2(1, 5)
+experiment_2(2, 5)
+experiment_2(3, 5)
+experiment_2(5, 5)
+experiment_2(5, 5)
+experiment_2(6, 5)
+experiment_2(7, 5)
+experiment_2(8, 5)
+experiment_2(9, 5)
+"""
